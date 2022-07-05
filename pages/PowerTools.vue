d1ef198cad6a5e4f679426e001af3e53e89a2ccf
keith hill <keith.hill@metastock.com>
2022-07-05 10:25:12 -0600

    initial commit, Powertools

---
+++ pages/PowerTools.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div class="container mx-auto">
+      <h1 class="text-3xl text-purple-700 font-extrabold">PowerTools page</h1>
+      <div class="Powertools
+                | grid grid-cols-1 md:grid-cols-2">
+        <div class="powertool sm:flex md:flex-col">
+          <div class="media">media</div>
+          <div class="blurb">blurb</div>
+        </div>
+        <div class="powertool sm:flex md:flex-col">
+          <div class="media">media</div>
+          <div class="blurb">blurb</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PowerToolsPage'
+}
+</script>
